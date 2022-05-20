2401d6a378dedef0690991a225bea7d61f70c4f3
Mat RF <matthieu.raynaud-de-fitte@outlook.com>
2022-05-20 21:48:09 +0200

    started implementing the set instruction

--- src/instructions/instruction_implementation/auto_translation/sub_instructions/set.py
+++ src/instructions/instruction_implementation/auto_translation/sub_instructions/set.py
@@ -6,7 +6,14 @@ class Set(InstructionParent):
         super(Set, self).__init__("Auto translation Set")
 
     def run(self, message):
+        split_message = message.strip().split(" ")[1:]
+        if len(split_message) == 0:
+            return "This instruction requires at least one langua"
         return "Auto translation set todo"
 
     def get_description(self) -> str:
-        return "Auto translation set description todo"
+        ret = "The set instruction tells the translator that you would like the current channel to be auto translated\n"
+        ret += "In order to use it, give one or more languages as parameter\n"
+        ret += "Example : `!ot auto-translate set fr en` sets the current channel to auto translate all messages to" \
+               "french and english"
+        return ret
