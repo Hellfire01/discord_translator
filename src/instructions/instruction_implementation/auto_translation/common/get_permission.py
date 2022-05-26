f6133cbeac4007a6b5d7f46b88fdbc67e6266c92
Mat RF <matthieu.raynaud-de-fitte@outlook.com>
2022-05-26 17:44:04 +0200

    bug correction

--- src/instructions/instruction_implementation/auto_translation/common/get_permission.py
+++ src/instructions/instruction_implementation/auto_translation/common/get_permission.py
@@ -2,5 +2,13 @@
 
 class GetPermission:
     @staticmethod
+    def __check_if_owner(message):
+        return message.author.id == message.guild.owner_id
+
+    @staticmethod
+    def __check_if_allowed_role(message):
+        return False
+
+    @staticmethod
     def check_if_allowed(message):
-        return True
+        return GetPermission.__check_if_owner(message) or GetPermission.__check_if_allowed_role(message)
