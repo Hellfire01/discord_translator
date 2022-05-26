941c8efde3ebe6f2ad8c8e297a11707a20b78bae
Mat RF <matthieu.raynaud-de-fitte@outlook.com>
2022-05-26 19:22:57 +0200

    added permission management for the get / set / remove isntructions

--- src/instructions/instruction_implementation/auto_translation/sub_instructions/get.py
+++ src/instructions/instruction_implementation/auto_translation/sub_instructions/get.py
@@ -1,3 +1,4 @@
+from src.instructions.instruction_implementation.comon.get_permission import GetPermission
 from src.instructions.instruction_inheritance import InstructionParent
 
 
@@ -8,6 +9,10 @@ class Get(InstructionParent):
         super(Get, self).__init__("Auto translation Get")
 
     def run(self, message):
+        if GetPermission.check_if_owner(message) is False:
+            ret = "I apologise but only the discord guild owner may use this instruction\n"
+            ret += "The owner of the discord guild can add allowed roles using the `" + self.commandline_config.first_keyword + " trusted-roles` instruction\n"
+            return ret
         channel = self.database_access.get_channel_instruction(message.channel.id)
         if channel is None:
             return "There is currently no auto translation enabled on the channel"
--- src/instructions/instruction_implementation/comon/get_permission.py
+++ src/instructions/instruction_implementation/comon/get_permission.py
@@ -3,7 +3,7 @@ from itertools import product
 
 class GetPermission:
     @staticmethod
-    def __check_if_owner(message):
+    def check_if_owner(message):
         return message.author.id == message.guild.owner_id
 
     @staticmethod
@@ -17,4 +17,4 @@ class GetPermission:
 
     @staticmethod
     def check_if_allowed(message, database_access):
-        return GetPermission.__check_if_owner(message) or GetPermission.__check_if_allowed_role(message, database_access)
+        return GetPermission.check_if_owner(message) or GetPermission.__check_if_allowed_role(message, database_access)
