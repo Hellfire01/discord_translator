6fa9d06ae7854810a38247e59f7936d8cff7b41f
Mat RF <matthieu.raynaud-de-fitte@outlook.com>
2022-05-26 19:20:26 +0200

    typos corrections

--- src/instructions/instruction_implementation/trusted_roles/sub_instructions/help.py
+++ src/instructions/instruction_implementation/trusted_roles/sub_instructions/help.py
@@ -19,8 +19,8 @@ class Help(InstructionParent):
         ret += "`" + self.commandline_config.first_keyword + " trusted-roles set [roles]` sets roles to be allowed to " \
                "change the auto translation settings\n"
         ret += "the **[roles]** needs to be replaced with the roles that are to be given permission\n"
-        ret += "usage example : `" + self.commandline_config.first_keyword + " trusted-roles set " \
-               "\\@auto-translation-role` this sets the users with the role \@auto-translation-role to be allowed to " \
+        ret += "usage example : `" + self.commandline_config.first_keyword + " trusted-roles set" \
+               " @auto-translation-role` this sets the users with the role \@auto-translation-role to be allowed to " \
                "edit the auto-translation settings\n"
         ret += "\n"
         ret += "`" + self.commandline_config.first_keyword + " auto-translate get` displays the roles that are allowed " \
@@ -29,7 +29,7 @@ class Help(InstructionParent):
         ret += "`" + self.commandline_config.first_keyword + " auto-translate remove [roles]` removes given roles from " \
                "the allowed to change the auto translation\n"
         ret += "Usage is the same as the `set` instruction\n"
-        ret += "Please note that the server owner will still be able to change te settings regardless of the roles he " \
+        ret += "Please note that the server owner will still be able to change the settings regardless of the roles he " \
                "has or has not\n"
         return ret
 
