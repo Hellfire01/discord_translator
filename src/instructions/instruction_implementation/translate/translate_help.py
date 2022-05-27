100deeec276f6f2a51677c7d29904013a1f24bee
Mat RF <matthieu.raynaud-de-fitte@outlook.com>
2022-05-27 18:09:33 +0200

    much better help description

--- src/instructions/instruction_implementation/translate/translate_help.py
+++ src/instructions/instruction_implementation/translate/translate_help.py
@@ -11,17 +11,23 @@ class TranslateHelp(InstructionParent):
         ret = "This is the `translate` option\n"
         ret += "The translation is made using google translate\n"
         ret += "The option is used for a direct translation between a given text and the expected output\n"
-        ret += "To use it, use the flag emotes, names or acronyms. The available flag emotes can be listed with the `" + \
+        ret += "To use it, use the names or acronyms. The available names and acronyms can be listed with the `" + \
                self.commandline_config.first_keyword + " lang-list` instruction\n"
-        ret += "The first flag is for the input languages, the following ones are the output languages\n"
+        ret += "\n"
+        ret += "The translator uses **" + self.translate_config.separator + "** in order to differentiate the 1 input " \
+               "language ( if there is any ) to the one or many output languages\n"
         ret += "\n"
         ret += "Usage example :\n"
-        ret += "**" + self.commandline_config.first_keyword + " translate en " + self.translate_config.separator + " french**\n"
-        ret += "This will attempt to take in english and translate it automatically to french\n"
+        ret += "**" + self.commandline_config.first_keyword + " translate en " + self.translate_config.separator + \
+               " french**\n"
+        ret += "**[text to translate]**\n"
+        ret += "This will attempt to take in english and translate it to french\n"
         ret += "\n"
         ret += "Usage example :\n"
-        ret += "**" + self.commandline_config.first_keyword + " translate " + self.translate_config.separator + " it**\n"
-        ret += "The translator will attempt to guess the input language and output it to italian\n"
+        ret += "**" + self.commandline_config.first_keyword + " translate " + self.translate_config.separator + \
+               " it es**\n"
+        ret += "**[text to translate]**\n"
+        ret += "The translator will attempt to guess the input language and translate it to italian and spanish\n"
         ret += "This may have mixed results if the input language is not correctly guessed though\n"
         return ret
 
